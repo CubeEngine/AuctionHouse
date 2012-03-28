d10e11e905dea64d9c3c57325fabf85cd1ba7b64
Phillip Schichtel <quick_wango@code-infection.de>
2012-03-29 00:25:41 +0300

    the some for Auction

--- src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Auction.java
+++ src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Auction.java
@@ -5,30 +5,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
+ * Represents an auction
  *
  * @author Anselm
  */
-public class Auction {
+public class Auction
+{
     public final int id;
     public final ItemStack item;
     public final Player owner;
     public final long auctionEnd;
     public final Stack<Bid> bids;
     
-    public Auction(int id,ItemStack item,Player owner,long auctionEnd){
+    public Auction(int id, ItemStack item, Player owner, long auctionEnd)
+    {
         this.id = id;
         this.item = item;
         this.owner = owner;
         this.auctionEnd = auctionEnd;
         this.bids = new Stack<Bid>();
     }
-    public boolean bid(Player bidder,double amount){
-        if (amount<this.bids.peek().amount){
+    
+    public boolean bid(Player bidder,double amount)
+    {
+        if (amount<this.bids.peek().amount)
+        {
             return false;
         }
-        else{
-            this.bids.push(new Bid(bidder,amount));
-            return true; 
-        }
+        this.bids.push(new Bid(bidder, amount));
+        return true; 
     }
 }
