a476a2f01d6e8f7c670fe4454b01b9eacd2193d0
Phillip Schichtel <quick_wango@code-infection.de>
2012-03-29 00:27:51 +0300

    made the properties private and added getter methods

--- src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Bid.java
+++ src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Bid.java
@@ -9,15 +9,30 @@ import org.bukkit.entity.Player;
  */
 public class Bid
 {
-    public final double amount;
-    public final Player bidder;
-    public final long timestamp;
+    private final double amount;
+    private final Player bidder;
+    private final long timestamp;
 
     public Bid(Player bidder, double amount)
     {
        this.amount = amount;
        this.bidder = bidder;
        this.timestamp = System.currentTimeMillis();
+    }
+    
+    public double getAmount()
+    {
+        return this.amount;
+    }
+    
+    public Player getBidder()
+    {
+        return this.bidder;
+    }
+    
+    public long getTimestamp()
+    {
+        return this.timestamp;
     }
 }
 
