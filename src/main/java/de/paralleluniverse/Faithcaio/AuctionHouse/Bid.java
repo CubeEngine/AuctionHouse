156af1b415c4dc2fdcb0ea1a5d6836f36ae489c4
Phillip Schichtel <quick_wango@code-infection.de>
2012-03-29 00:23:59 +0300

    changed style and added documentation

--- src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Bid.java
+++ src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/Bid.java
@@ -3,15 +3,18 @@ package de.paralleluniverse.Faithcaio.AuctionHouse;
 import org.bukkit.entity.Player;
 
 /**
+ * Represents a bid by a player
  *
  * @author Anselm
  */
-public class Bid {
+public class Bid
+{
     public final double amount;
     public final Player bidder;
     public final long timestamp;
 
-    public Bid(Player bidder,double amount){
+    public Bid(Player bidder, double amount)
+    {
        this.amount = amount;
        this.bidder = bidder;
        this.timestamp = System.currentTimeMillis();
