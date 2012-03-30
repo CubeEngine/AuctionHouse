d3d36976e024418adb01ec89c1cd2a1f3680b933
Anselm Brehme <Faithcaio@yahoo.de>
2012-03-30 02:20:56 +0200

    added some configs

--- src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/AuctionHouseConfiguration.java
+++ src/main/java/de/paralleluniverse/Faithcaio/AuctionHouse/AuctionHouseConfiguration.java
@@ -1,19 +1,31 @@
 package de.paralleluniverse.Faithcaio.AuctionHouse;
 
+import java.util.List;
 import org.bukkit.configuration.Configuration;
 
 public class AuctionHouseConfiguration
 {
-    public final int player_maxBet;
-    public final boolean opIgnoreMaxBet;
-    public final int auction_undoTimer; //In Seconds (-1 for Infinite)
-    public final int auction_maxAuctions;
-   
+
+
+    public final int      auction_undoTime;            //In seconds (-1 = Infinite)
+    public final int      auction_maxAuctions;          //Overall
+    public final int      auction_maxAuctions_player;   //per Player
+    public final boolean  auction_maxAuctions_opIgnore; //Op ignore perPlayer limit NOT Overall Limit!
+    public final int      auction_maxLength;            //in hours
+    public final boolean  auction_opCanCheat;           //Op can Cheat Items for Auction
+    public final List<String> auction_blacklist;        //Blacklist Materials
+    //TODO blacklist einbauen
     public AuctionHouseConfiguration(Configuration config)
     {
-        this.player_maxBet = config.getInt("player.maxBet");
-        this.opIgnoreMaxBet = config.getBoolean("opIgnoreMaxBet");
-        this.auction_undoTimer = config.getInt("auction.UndoTimer");
+        this.auction_maxAuctions_player = config.getInt("auction.maxAuctions.player");
+        this.auction_maxAuctions_opIgnore = config.getBoolean("auction.maxAuctions.opIgnore");
+        this.auction_undoTime = config.getInt("auction.undoTime");
         this.auction_maxAuctions = config.getInt("auction.maxAuctions");
+        this.auction_maxLength = config.getInt("auction.maxLength");
+        this.auction_opCanCheat = config.getBoolean("auction.opCanCheat");
+        this.auction_blacklist = config.getStringList("auction.blacklist");
+        //TODO autocreate default Config
+        //TODO Preis fuer AuktionsErstellung (Formel mit Startgebot?)
+        
     }
 }
